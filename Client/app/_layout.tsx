0ec760b78b6924b1f4fb11e5c19f9bc72e1c3939
Gobihanath Balasubramaniam <126072422+Gobihanath@users.noreply.github.com>
2024-06-29 20:43:54 +0530

    Font loading error resolved

--- Client/app/_layout.tsx
+++ Client/app/_layout.tsx
@@ -1,45 +1,54 @@
-
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect, useState } from 'react';
 import { View } from 'react-native';
-import OnBoarding from './(routes)/onboarding';
-
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    'outfit': require('../assets/fonts/Outfit-Regular.ttf'),
+    'outfit-bold': require('../assets/fonts/Outfit-Bold.ttf'),
+    'outfit-medium': require('../assets/fonts/Outfit-Medium.ttf'),
+    'outfit-black': require('../assets/fonts/Outfit-Black.ttf'),
+    'outfit-semibold': require('../assets/fonts/Outfit-SemiBold.ttf'),
+    'outfit-thin': require('../assets/fonts/Outfit-Thin.ttf'),
+    'outfit-light': require('../assets/fonts/Outfit-Light.ttf'),
+  });
 
-  useFonts({
-    'outfit':require('../assets/fonts/Outfit-Regular.ttf'),
-    'outfit-bold':require('../assets/fonts/Outfit-Bold.ttf'),
-    'outfit-medium':require('../assets/fonts/Outfit-Medium.ttf'),
-    'outfit-black':require('../assets/fonts/Outfit-Black.ttf'),
-    'outfit-semibold':require('../assets/fonts/Outfit-SemiBold.ttf'),
-    'outfit-thin':require('../assets/fonts/Outfit-Thin.ttf'),
-    'outfit-light':require('../assets/fonts/Outfit-Light.ttf'),
-});
+  const [appIsReady, setAppIsReady] = useState(false);
 
-  
+  useEffect(() => {
+    async function prepare() {
+      await SplashScreen.preventAutoHideAsync();
 
-  const[isLoggedIn,setIsLoggedIn] = useState(false)
+      if (fontsLoaded) {
+        setAppIsReady(true);
+        await SplashScreen.hideAsync();
+      }
+    }
+
+    prepare();
+  }, [fontsLoaded]);
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  if (!appIsReady) {
+    return null; // Don't render anything until fonts are loaded
+  }
 
   return (
     <>
-
-    {isLoggedIn?(
-      <View>
-
-      </View>
-
-    ):
-    <Stack screenOptions={{headerShown:false}}>      
-      <Stack.Screen name="index"/>
-      <Stack.Screen name="(routes)/welcome-intro/index" />
-      <Stack.Screen name="(routes)/login/index" />  
-      <Stack.Screen name="(routes)/sign-up/index" />  
-      <Stack.Screen name="(tabs)" />    
-    </Stack>
-    }
+      {isLoggedIn ? (
+        <View></View>
+      ) : (
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="index" />
+          <Stack.Screen name="(routes)/welcome-intro/index" />
+          <Stack.Screen name="(routes)/login/index" />
+          <Stack.Screen name="(routes)/sign-up/index" />
+          <Stack.Screen name="(tabs)" />
+        </Stack>
+      )}
     </>
   );
 }
